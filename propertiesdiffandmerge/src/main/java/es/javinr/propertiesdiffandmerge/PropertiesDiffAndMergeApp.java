98fbd9189dc25eaa33449ec72f6410b16b5961fc
javinr <javinr@gmail.com>
2016-02-25 15:58:38 +0100

    Output to file

--- propertiesdiffandmerge/src/main/java/es/javinr/propertiesdiffandmerge/PropertiesDiffAndMergeApp.java
+++ propertiesdiffandmerge/src/main/java/es/javinr/propertiesdiffandmerge/PropertiesDiffAndMergeApp.java
@@ -3,6 +3,7 @@ package es.javinr.propertiesdiffandmerge;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -10,6 +11,7 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang3.tuple.Pair;
@@ -19,7 +21,36 @@ import org.apache.commons.lang3.tuple.Pair;
  *
  */
 public class PropertiesDiffAndMergeApp {
+	public boolean isOutputToFile() {
+		return outputToFile;
+	}
+
+	public void setOutputToFile(boolean outputToFile) {
+		this.outputToFile = outputToFile;
+	}
+
+	public File getFile1() {
+		return originalFile;
+	}
+
+	public void setFile1(File file1) {
+		this.originalFile = file1;
+	}
+
+	public File getFile2() {
+		return modifiedFile;
+	}
+
+	public void setFile2(File file2) {
+		this.modifiedFile = file2;
+	}
+
 	private static final Options options;
+	private boolean outputToFile = false;
+	private File outputFile;
+	private File originalFile;
+	private File modifiedFile;
+	
 	static {
 		options = createOptions();
 	}
@@ -32,50 +63,80 @@ public class PropertiesDiffAndMergeApp {
 				use();
 				System.exit(-2);
 			}
-			String file1Path = commandLine.getArgs()[0];
-			String file2Path = commandLine.getArgs()[1];
-			File file1 = new File(file1Path);
-			File file2 = new File(file2Path);
+			String originalFilePath = commandLine.getArgs()[0];
+			String modifiedFile2Path = commandLine.getArgs()[1];
+			File originalFile = new File(originalFilePath);
+			File modifiedFile = new File(modifiedFile2Path);
 			boolean error = false;
 			StringBuilder sbError = new StringBuilder();
-			if (!file1.exists()) {
+			if (!originalFile.exists()) {
 				error = true;
-				sbError.append("File ").append(file1Path).append(" does not exists\n");
+				sbError.append("File ").append(originalFilePath).append(" does not exists\n");
 			}
-			if (!file2.exists()) {
+			if (!modifiedFile.exists()) {
 				error = true;
-				sbError.append("File ").append(file2Path).append(" does not exists\n");
+				sbError.append("File ").append(modifiedFile2Path).append(" does not exists\n");
 			}
 			if (error) {
 				System.err.println(sbError);
 				use();
 				System.exit(-1);
 			}
-			System.out.println("Diffing " + file1Path + " and " + file2Path);
-			Properties properties1 = new Properties();
-			Properties properties2 = new Properties();
-			properties1.load(new FileInputStream(file1));
-			properties2.load(new FileInputStream(file2));
-			Properties propertiesIn1NotIn2 = diffPropertiesNotIn(properties1, properties2);
-			Properties propertiesIn2NotIn1 = diffPropertiesNotIn(properties2, properties1);
-			Properties propertiesDifferent = diffProperties(properties1, properties2);
-			showProperties("Properties in " + file1Path + " not in " + file2Path + ":", propertiesIn1NotIn2);
-			showProperties("Properties in " + file2Path + " not in " + file1Path + ":", propertiesIn2NotIn1);
-			showPairProperties("Different properties:", propertiesDifferent);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
+			PropertiesDiffAndMergeApp app = new PropertiesDiffAndMergeApp();
+			app.setFile1(originalFile);
+			app.setFile2(modifiedFile);
+			if (commandLine.hasOption('o')) {
+				app.setOutputToFile(true);
+				app.setOutputFile(new File(commandLine.getOptionValue('o')));
+			}
+			System.out.println("Diffing " + originalFilePath + " and " + modifiedFile2Path);
+			app.run();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	public void run() throws Exception {
+		Properties properties1 = loadFile(originalFile);
+		Properties properties2 = loadFile(modifiedFile);
+		
+		Properties propertiesIn1NotIn2 = diffPropertiesNotIn(properties1, properties2);
+		Properties propertiesIn2NotIn1 = diffPropertiesNotIn(properties2, properties1);
+		Properties propertiesDifferent = diffProperties(properties1, properties2);
+		showProperties("Properties in " + originalFile.getName() + " not in " + modifiedFile.getName() + ":", propertiesIn1NotIn2);
+		showProperties("Properties in " + modifiedFile.getName() + " not in " + originalFile.getName() + ":", propertiesIn2NotIn1);
+		showPairProperties("Different properties:", propertiesDifferent);
+		if (outputToFile) {
+			Properties outputProperties = new Properties();
+			outputProperties.putAll(properties2);
+			try (FileWriter outputFileWriter = new FileWriter(getOutputFile())) {
+				outputProperties.store(outputFileWriter, null);
+			} catch (IOException e) {
+				throw new Exception("Error writing output file " + getOutputFile().getAbsolutePath(), e);
+			}
 		}
 	}
 
+	private Properties loadFile(File file) throws Exception {
+		Properties properties = new Properties();
+		try (FileInputStream fis = new FileInputStream(file)) {
+			properties.load(fis);
+		} catch (FileNotFoundException e) {
+			throw new Exception("File " + originalFile.getAbsolutePath() + " not found", e);
+		} catch (IOException e) {
+			throw new Exception("Error readin file " + originalFile.getAbsolutePath(), e);
+		}
+		return properties;
+	}
 	private static Options createOptions() {
 		Options options = new Options();
+		options.addOption(Option.builder("o")
+				.longOpt("output")
+				.hasArg()
+				.argName("output file")
+				.required(false)
+				.desc("Output file in wich write merge results")
+				.build());
 		return options;
 	}
 
@@ -126,5 +187,13 @@ public class PropertiesDiffAndMergeApp {
 		HelpFormatter helpFormatter = new HelpFormatter();
 		helpFormatter.printHelp("PropertiesDiffAndMergeApp file1 file2", options);
 		
+	}
+
+	public File getOutputFile() {
+		return outputFile;
+	}
+
+	public void setOutputFile(File outputFile) {
+		this.outputFile = outputFile;
 	}
 }
